348149f4f7245e0e21f114ca2181768c762f3902
F.Prieto <felipe00@live.com.mx>
2020-12-15 01:38:09 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -17,3 +17,10 @@ results = chi2_contingency([
 p = results[1]
 
 # Paste Code Here:
+def repeat_stuff(stuff, num_repeats=10):
+  return stuff*num_repeats
+
+lyrics = repeat_stuff("Row ", 3) + "Your Boat. "
+song = repeat_stuff(lyrics)
+
+print(song)
