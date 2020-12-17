7e4df8989e42e31afa2bc452be8868ca6dac574a
F.Prieto <felipe00@live.com.mx>
2020-12-17 00:54:38 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -37,3 +37,19 @@ names_and_dogs_names = zip(names,dogs_names)
 #To print it´s neccesary convert the object into a list
 print(list(name_and_dogs_names))
 #We can add a single element to a list using .append(). 
+#If we want to add a single element using +, we have to put it into a list with brackets ([]):
+my_list + [4]
+#Other example list
+items_sold_new = items_sold + ['biscuit', 'tart']
+>>> print(items_sold_new)
+['cake', 'cookie', 'bread', 'biscuit', 'tart']
+#Just like with zip, the range function returns an object that we can convert into a list:
+print(my_range)
+range(0, 10)
+>>> print(list(my_range))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# If we use a third argument, we can create a list that “skips” numbers. For example, range(2, 9, 2) will give us a list where each number is 2 greater than the previous number:
+my_range2 = range(2, 9, 2)
+>>> print(list(my_range2))
+[2, 4, 6, 8]
+#Es el rango y va saltando conforme lo deseemos
