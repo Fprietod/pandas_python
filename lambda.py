9ffd22f151080d9309f1900675048c7ecfaa2cb5
F.Prieto <felipe00@live.com.mx>
2020-12-18 20:00:55 -0600

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,9 @@
+#lambda function is a one-line shorthand for function.
+add_two = lambda my_input: my_input + 2
+print(add_two(3))
+print(add_two(100))
+print(add_two(-2))
+#The print
+ 5
+>>> 102
+>>> 0
