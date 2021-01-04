74ecf870d109b5a0c4bcb5d22dd3aa8daf0014b3
F.Prieto <felipe00@live.com.mx>
2021-01-04 00:34:22 -0600

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+menu = {"oatmeal": 3, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+#A dictionary begins and ends with curly braces ({ and }).
+#Each item consists of a key (i.e., “oatmeal”) and a value (i.e., 3)
+#Each key: value pair (i.e., "oatmeal": 3 or "avocado toast": 6) is separated by a comma (,)
+#It’s considered good practice to insert a space () after each comma, but your code will still run without the space.
+#Dictionaries provide us with a way to map pieces of data to each other, so that we can quickly find values that are associated with one another.
+#However, the keys can be numbers as well. For example, if we were mapping restaurant bill subtotals to the bill total after tip
+subtotal_to_total = {20: 24, 10: 12, 5: 6, 15: 18}
+#Values can be any type. You can use a string, a number, a list, or even another dictionary as the value associated with a key!
+students_in_classes = {"software design": ["Aaron", "Delila", "Samson"], "cartography": ["Christopher", "Juan", "Marco"], "philosophy": ["Frederica", "Manuel"]}
+#You can also mix and match key and value types. For example:
+person = {"name": "Shuri", "age": 18, "siblings": ["T'Chaka", "Ramonda"]}
