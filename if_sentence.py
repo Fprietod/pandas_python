66eea10a89abf8dd52ae348fd78df8cd844de40b
F.Prieto <felipe00@live.com.mx>
2021-01-09 02:30:41 -0600

    Update if_sentence.py

--- if_sentence.py
+++ if_sentence.py
@@ -9,3 +9,27 @@ statement_two = (4 * 2 <= 8) and (7 - 1 == 6)
 def graduation_reqs(credits,gpa):
   if credits >= 120 and gpa >=2.0:
     return "You meet the requirements to graduate!"
+
+  # OR
+  #Oranges are a fruit or apples are a vegetable.
+  #This statement is composed of two expressions: oranges are a fruit which is True and apples are a vegetable which is False.
+  #Because the two expressions are connected by the or operator, the entire statement is True. Only one component needs to be True for an or statement to be True.
+  
+  #Operator Not
+  #So if we have a True statement and apply a not operator we get a False statement.
+  #not True == False
+  #not False == True
+  >>> not 1 + 1 == 2
+False
+>>> not 7 < 0
+True
+#####################
+def graduation_reqs(gpa, credits):
+  if (gpa >= 2.0) and (credits >= 120):
+    return "You meet the requirements to graduate!"
+  if (gpa >= 2.0) and not (credits >= 120):
+    return "You do not have enough credits to graduate."
+  if not (gpa >= 2.0) and (credits >= 120):
+    return "Your GPA is not high enough to graduate."
+  if not (gpa >= 2.0) and not (credits >= 120):
+    return "You do not meet either requirement to graduate!"
