7d4438f00158c6580653b717b5c808c2089c3629
F.Prieto <felipe00@live.com.mx>
2021-01-16 20:22:42 -0600

    Update list.py

--- list.py
+++ list.py
@@ -13,3 +13,10 @@ grades.append(100)
 
 #If you want to add two list
 full_gradebook = gradebook + last_semester_gradebook
+
+#Range II
+#With one or two arguments, range will create a list of consecutive numbers (i.e., each number is one greater than the previous number). If we use a third argument, we can create a list that “skips” numbers.
+# For example, range(2, 9, 2) will give us a list where each number is 2 greater than the previous number:
+>>> my_range2 = range(2, 9, 2)
+>>> print(list(my_range2))
+[2, 4, 6, 8]
