c2dd964dcbdd1e8bbb382bfc487ad07d9d117f17
F.Prieto <felipe00@live.com.mx>
2021-01-16 19:38:47 -0600

    Create clipper.py

---
+++ clipper.py
@@ -0,0 +1,17 @@
+#Carly's Clippers
+
+#You are the Data Analyst at Carly’s Clippers, the newest hair salon on the block. Your job is to go through the lists of data that have been collected in the past couple of weeks. You will be calculating some important metrics that Carly can use to plan out the operation of the business for the rest of the month.
+
+#You have been provided with three lists:
+
+   # hairstyles: the names of the cuts offered at Carly’s Clippers
+   # prices: the price of each hairstyle in the hairstyles list
+   # last_week: the number of each hairstyle in hairstyles that was purchased last week
+  
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+total_price = 0
