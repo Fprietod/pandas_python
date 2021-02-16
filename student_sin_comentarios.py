0b8148875332011c4393b4f59d5e79629c5fca47
F.Prieto <felipe00@live.com.mx>
2021-02-15 19:39:16 -0600

    Update student_sin_comentarios.py

--- student_sin_comentarios.py
+++ student_sin_comentarios.py
@@ -1,3 +1,4 @@
+from typing import List
 def average_students(items: List[List[int]]) -> List[List[int]]:
   items.sort(reverse = True)
   resultado = []
@@ -9,15 +10,15 @@ def average_students(items: List[List[int]]) -> List[List[int]]:
   #Promediarlo
   for i , j in items:
 
-    if i == index_lista:
-      if len(suma_id) < 5:
+    if i == index_lista and index_lista <=1000:
+      if len(suma_id) < 5 and j <=1000:
         suma_id.append(j)
     else:
       resultado.append([index_lista,sum(suma_id) // len(suma_id)])
       suma_id = [j]
       index_lista = i
       resultado = resultado[::-1]
-     
-     
 
-      return resultado
+
+      if resultado[0][1] <= 100:
+        return resultado
